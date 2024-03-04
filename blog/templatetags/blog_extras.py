80eb978cc715cbe4fae7dc38277aadd4f56a3d3a
As PIE <gyorgy.bakocs@transprnt.com>
2024-03-04 09:17:52 +0000

    Finish crispy forms

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -19,6 +19,14 @@ def row(extra_classes=""):
 def endrow():
     return format_html("</div>")
 
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
 
 @register.simple_tag(takes_context=True)
 def author_details_tag(context):
